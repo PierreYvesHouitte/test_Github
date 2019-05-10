e0c3c86d6be38191fdb98471088636959de4b478
PierreYvesHouitte <pyhouitte@gmail.com>
2019-05-10 16:07:54 +0200

    calcul du yaw

--- hello.cc
+++ hello.cc
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
+
+#define PI 3.14159265359
 
 using namespace std;
 
 int main()
 {
-	for(int i=0; i<5;i++)
+	for(int i=-4; i<5;i++)
 	{
-		float r = ((float) rand() / RAND_MAX);
-		float test = r * (4 - -4) + -4;
-		cout << test << "\n";
-		cout << rand() << "\n";
+		for(int j=-4; j<5; j++)
+		{
+			int signj = (j > 0) - (j<0);
+			int yaw = signj*acos(i/sqrt(pow(i,2)+pow(j,2)))*180/PI; 
+
+			cout << "signy:" << signj << " x:" << i <<" y:" << j << " yaw:" << yaw  << "\n";
+			
+		}
 	}
 	
  
 	
-		return 0;
+	return 0;
 }
