08a5c2b931871faeba7fe3a1c4f89f4f13721da5
PierreYvesHouitte <pyhouitte@gmail.com>
2019-05-10 09:36:23 +0200

    test gitignore

--- hello.cc
+++ hello.cc
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
-
 {
-	cout << "Hello, world!\n";
-	return 0;
+	for(int i=0; i<5;i++)
+	{
+		float r = ((float) rand() / RAND_MAX);
+		float test = r * (4 - -4) + -4;
+		cout << test << "\n";
+		cout << rand() << "\n";
+	}
+	
+ 
+	
+		return 0;
 }
-
